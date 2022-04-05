7b7038e5c8020c77c4b6a64e0b0ec61ce614bc84
Josuepma <1930168@upv.edu.mx>
2022-04-05 07:37:09 -0500

    ruta home a directorio raiz

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'It Solutions') }}</title>
+        <title>{{ config('app.name', 'Instagram clone') }}</title>
 
 
         <!-- Styles -->
@@ -48,7 +48,7 @@
                     @if (Route::has('login'))
                         <div>
                             @auth
-                                <a href="{{ url('/home') }}">Home</a>
+                                <a href="{{ url('/') }}">Home</a>
                             @else
                                 <a role="button" class="btn btn-outline-success" href="{{ route('login') }}">{{ __('Login') }}</a>
 
