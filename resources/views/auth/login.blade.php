7b680da0fc82b3327557dcc2a9be7740b53b0598
Jordy Lagunas Higuera <1930136@upv.edu.mx>
2022-04-09 11:46:22 -0500

    Comenzando diseno de login y de register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,20 +5,18 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
                                            class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                           value="{{ old('email') }}" required autocomplete="email"
+                                           placeholder="Phone number,username, or eamil" autofocus>
 
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,13 +27,11 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                            class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="current-password">
+                                           required autocomplete="current-password" placeholder="Password">
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
