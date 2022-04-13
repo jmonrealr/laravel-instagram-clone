c0f1e23033a6e18def0f990024385d1cef4dbda3
Josuepma <1930168@upv.edu.mx>
2022-04-13 18:28:52 -0500

    añadiendo perfil al controlador

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Profile;
 
 class HomeController extends Controller
 {
 
     public function index(){
         $posts = Post::with(['likes','comments','images','user'])->orderByDesc('created_at')->get();
-        $users = User::all();
+        $users = Profile::with('user')->get();
+
         return view('home.index',get_defined_vars());
     }
 
