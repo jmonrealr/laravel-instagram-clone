c6012ba83e18221fd4c5da69b021c42193e7c0f8
Josuepma <1930168@upv.edu.mx>
2022-04-13 18:41:53 -0500

    fix

--- database/migrations/2022_04_13_215130_create_profiles_table.php
+++ database/migrations/2022_04_13_215130_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProfileTable extends Migration
+class CreateProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -28,6 +28,6 @@ class CreateProfileTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profile');
+        Schema::dropIfExists('profiles');
     }
 }
