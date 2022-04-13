da1f9e696b90f107a724b1ddadfb9ce1e265c97c
Crxwler <loco7139@gmail.com>
2022-04-13 16:57:03 -0500

    add: user profile event

---
+++ app/Providers/CreateNewProfile.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Symfony\Component\HttpKernel\Profiler\Profile;
+
+class CreateNewProfile
+{
+    use SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $user = $event->user;
+        Profile::create(['user_id' => $user->id]);
+    }
+}
---
+++ database/migrations/2022_04_13_215130_create_profile_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->text('url_image')->nullable();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
