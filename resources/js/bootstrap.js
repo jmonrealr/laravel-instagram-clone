3b2cfceb10f0c689da5203c06fb2897367734375
Crxwler <loco7139@gmail.com>
2022-04-13 14:09:48 -0500

    add: generic alerts with session problem

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,8 @@
 window._ = require('lodash');
 import "bootstrap";
+try {
+    window.iziToast = require('izitoast');
+}catch (e) {}
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
