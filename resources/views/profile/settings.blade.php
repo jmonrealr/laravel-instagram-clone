3f6a0d651d5d15f7d5ead55031ca17bc04e4271e
Jordy Lagunas Higuera <1930136@upv.edu.mx>
2022-04-17 22:08:33 -0500

    Settings

--- resources/views/profile/settings.blade.php
+++ resources/views/profile/settings.blade.php
@@ -37,6 +37,35 @@
                                     <div class="col-8">
                                         <div class="row" style="margin-top: 10px;">
                                             <label for="exampleInputEmail1" class="form-label ">Full name</label>
+                                            <br>
+                                            <!-- Button trigger modal -->
+                                            <a href="" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Ver"data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                             style="text-decoration: none; color:black;">Cambiar foto
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-body">
+                                                            <form>
+                                                                <div class="row">
+                                                                    <div class="col-12 justify-content-center" style="margin-top: 10px;">
+                                                                        <img src="{{ asset('images/jordy.png') }}" alt="Foto" class="justify-content-center " style="position:absolute;">
+                                                                    </div>
+                                                                    <div class="col-12" style="margin-top: 10px;">
+                                                                        <div class="input-group custom-file-button">  
+                                                                             
+                                                                            <input type="file" class="form-control" id="inputGroupFile">
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <button type="submit" class="btn btn-primary justify-content-center" style="margin-top: 10px;">Submit</button>
+                                                            </form>
+                                                        
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                         <div class="row" style="margin-top: 10px;">
                                             <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
